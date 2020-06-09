0c97d7fb0d161e28625d739070b8398cdfd15097
Gboriss <galinaborissenko@gmail.com>
2020-06-09 15:59:03 +0300

    localData

--- src/store.js
+++ src/store.js
@@ -1,37 +1,48 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import localData from './modules/localData.js'
+
 Vue.use(Vuex)
 
 import { todos } from './seed.js'
 
 export default new Vuex.Store({
     state: {
-        todos: todos
+        todos: localData().get()
     },
 
     mutations: {
         addTask: (state, data) => {
             state.todos.push(data)
+            localData().set(state.todos)
         },
 
         editTodo: (state, id) => {
             let todo = state.todos.find(todo => 
                 (todo.id === id))
             todo.edit = true
+            localData().set(state.todos)
         },
 
         remove: (state, id) => {
-            let todo = state.todos.find(todo => 
+            let todo = state.todos.findIndex(todo => 
                 (todo.id === id))
 
-            state.todos.splice(todo, 1)
+                state.todos = [
+                    ...state.todos.slice(0, todo),
+                    ...state.todos.slice(todo + 1)
+                ]
+
+            // state.todos.splice(todo, 1)
+            localData().set(state.todos)
         },
 
         check: (state, id) => {
             let todo = state.todos.find(todo => 
                 (todo.id === id))
             todo.completed = !todo.completed
+            localData().set(state.todos)
         }
     }
 })
